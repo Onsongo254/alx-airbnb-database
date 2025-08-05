95a9eab38592ea3f56f90c5b73d65211b491d673
Onsongo <me@localhost.com>
2025-08-05 05:55:42 -0700

    Added ORDER BY clause to the LEFT JOIN query in joins_queries.sql to sort properties by name.

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -7,7 +7,8 @@ INNER JOIN User u ON b.user_id = u.user_id;
 SELECT p.property_id, p.name AS property_name, p.location,
        r.review_id, r.rating, r.comment, r.created_at AS review_date
 FROM Property p
-LEFT JOIN Review r ON p.property_id = r.property_id;
+LEFT JOIN Review r ON p.property_id = r.property_id
+ORDER BY p.name;
 
 SELECT u.user_id, u.first_name, u.last_name, u.email,
        b.booking_id, b.property_id, b.start_date, b.end_date, b.status
